4347aa4ef65103c054e1f35da2a977c36b50b21e
LostKoe <oscanero3@gmail.com>
2025-09-30 21:00:27 -0300

    Cards

--- dist/main.js
+++ dist/main.js
@@ -1,9 +1,7 @@
 import { onHover } from "../js/events/on-hover.js"
-import { alerts } from "../js/responsivity/alerts.js"
 
 document.addEventListener("DOMContentLoaded", () => {
     const elementsAnimationTime = document.querySelectorAll("[data-ls-anim-time]")
 
     onHover()
-    alerts()
 })
