4e6111ac519b4113b28bb901660159a602605631
LostKoe <oscanero3@gmail.com>
2025-10-23 11:08:54 -0300

    Component: Navbars

--- dist/main.js
+++ dist/main.js
@@ -1,11 +1,13 @@
 import { onHover } from "../js/events/on-hover.js"
-import { divisors } from "../js/styles/divisors.js"
 import { dropdowns } from "../js/events/dropdown.js"
+import { divisors } from "../js/styles/divisors.js"
+import { navbar } from "../js/responsivity/navbars.js"
 
 document.addEventListener("DOMContentLoaded", () => {
     const elementsAnimationTime = document.querySelectorAll("[data-ls-anim-time]")
 
     onHover()
-    divisors()
     dropdowns()
+    divisors()
+    navbar()
 })
