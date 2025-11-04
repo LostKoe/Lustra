9aa8cfb6e0e93c54f313d379728b63b4de9b0fb1
LostKoe <oscanero3@gmail.com>
2025-11-04 20:22:06 -0300

    Toggle buttons

--- code/js/components/btn.js
+++ code/js/components/btn.js
@@ -1,34 +1,42 @@
 export function LustraBtns() {
-    const checkboxBtnsParent = document.querySelectorAll(".btn-checkbox-group");
+    const checkboxBtnsParent = document.querySelectorAll(".btn-checkbox-group")
 
     checkboxBtnsParent.forEach((parent) => {
-        const checkboxBtns = parent.querySelectorAll(".btn-checkbox");
+        const checkboxBtns = parent.querySelectorAll(".btn-checkbox")
 
         checkboxBtns.forEach((children) => {
-            children.style.cursor = "pointer";
+            children.style.cursor = "pointer"
 
             children.addEventListener("click", () => {
-                children.classList.toggle("btn-active");
-            });
-        });
-    });
+                children.classList.toggle("btn-active")
+            })
+        })
+    })
 
-    const radioBtnsParent = document.querySelectorAll(".btn-radio-group");
+    const radioBtnsParent = document.querySelectorAll(".btn-radio-group")
 
     radioBtnsParent.forEach((parent) => {
-        const radioBtns = parent.querySelectorAll(".btn-radio");
+        const radioBtns = parent.querySelectorAll(".btn-radio")
 
         radioBtns.forEach((children) => {
-            children.style.cursor = "pointer";
+            children.style.cursor = "pointer"
 
             children.addEventListener("click", () => {
                 if (children.classList.contains("btn-active")) {
-                    children.classList.remove("btn-active");
+                    children.classList.remove("btn-active")
                 } else {
-                    radioBtns.forEach((btn) => btn.classList.remove("btn-active"));
-                    children.classList.add("btn-active");
+                    radioBtns.forEach((btn) => btn.classList.remove("btn-active"))
+                    children.classList.add("btn-active")
                 }
-            });
-        });
-    });
+            })
+        })
+    })
+
+    const toggleBtn = document.querySelectorAll(".btn[data-ls-toggle='button']")
+
+    toggleBtn.forEach((element), () => {
+        element.addEventListener("click", () => {
+            element.classList.toggle("btn-active")
+        })
+    })
 }
